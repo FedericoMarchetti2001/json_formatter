6c75061504e1441b3531972886de8480bdfec010
Federico_Marchetti <federico.marchetti@edu.itspiemonte.it>
2025-07-21 21:25:03 +0200

    FEATURE: Adding TAB event listener

--- src/pages/Home/components/Pagination/index.tsx
+++ src/pages/Home/components/Pagination/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import { Tabs, Tab } from "@mui/material";
 
@@ -13,7 +13,18 @@ export default function FormatterPagination(
   props: IFormatterPaginationProps
 ): React.ReactElement<IFormatterPaginationProps> {
   return (
-    <Tabs value={props.currentPage} variant="scrollable" scrollButtons="auto">
+    <Tabs
+      value={props.currentPage}
+      variant="scrollable"
+      scrollButtons="auto"
+      onKeyDown={(event) => {
+        if (event.key === "Tab") {
+          event.preventDefault();
+          const nextPageIndex = (props.currentPage % props.totalPageCount) + 1;
+          props.setCurrentPage(nextPageIndex);
+        }
+      }}
+    >
       {Array.from({ length: props.totalPageCount }, (_, i) => (
         <Tab
           key={i + 1}
